022756d8afd68a2257d81f1781b24263ba39b824
Alexandru Cernat <alexandru.cernat@manchester.ac.uk>
2020-06-10 14:20:28 +0100

    set-up

---
+++ scripts/01.master.R
@@ -0,0 +1,24 @@
+
+
+# Set-up  -----------------------------------------------------------------
+
+# load packages
+library(tidyverse)
+library(lubridate)
+
+
+
+
+# Creat folder structure --------------------------------------------------
+
+## run just once
+dir.create("./data")
+dir.create("./data/raw")
+dir.create("./data/clean")
+dir.create("./output")
+dir.create("./output/fig")
+dir.create("./output/tab")
+dir.create("./documentation")
+dir.create("./paper")
+dir.create("./scripts")
+
